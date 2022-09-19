41cb164d223f6122ef88bdac4f70eba0dee27526
rachellima <Lima2014!>
2022-09-19 12:44:00 -0300

    add sample
    Signed-off-by: rachellima <Lima2014!>

---
+++ fluttermodule/lib/screens/alternative_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class AlternativeScreen extends StatelessWidget {
+  const AlternativeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AlternativeSplashScreen(),
+    );
+  }
+}
+
+class AlternativeSplashScreen extends StatelessWidget {
+  const AlternativeSplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text('Hello from flutter')
+      ),
+    );
+  }
+}
